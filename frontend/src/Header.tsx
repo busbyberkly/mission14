9ca2d4b0707fd489896a13f5cfe9f62e5707c648
busbyberkly <bzpeterson0901@gmail.com>
2023-04-04 23:23:12 -0600

    frontend stuff

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,16 @@
+import scooby from "./download.png";
+
+function Header(props: any) {
+  return (
+    <header className="row">
+      <div className="col-4">
+        <img src={scooby} alt="Scooby Doo" />
+      </div>
+      <div className="col">
+        <h3>{props.slogan}</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
